fffbf6439dd4099ff41c0390baf3a690d1341a0c
Gabriela Caeiro <gabriela.caeiro.topa@gmail.com>
2023-05-13 23:54:32 +0200

    added css

---
+++ app.js
@@ -0,0 +1,15 @@
+//Way to generate random HEX in VanillaJS
+
+/* function generateHex(){
+    const letters = '0123456789ABCDEF';
+    let hash = '#';
+    for (let i = 0; i < 6; i++) {
+        hash += letters[Math.floor(Math.random() * 16)];
+    }
+    return hash;
+}
+let randomHex = generateHex(); */
+
+//Global selections and variables
+
+//Functions
