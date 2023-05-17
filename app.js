910fa97927b6e0634e84eef5c22eb03aa5ccfdb3
Gabriela Caeiro <gabriela.caeiro.topa@gmail.com>
2023-05-17 09:31:06 +0200

    updated text and contrast

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ let initialColors;
 sliders.forEach((slider) => {
 	slider.addEventListener("input", hslControls);
 });
+colorDivs.forEach((div, index) => {
+	div.addEventListener("change", () => {
+		updateTextUi(index);
+	});
+});
 
 //Functions
 
@@ -97,5 +102,20 @@ function hslControls(e) {
 		.set("hsl.l", brightness.value)
 		.set("hsl.h", hue.value);
 	colorDivs[index].style.backgroundColor = color;
+}
+
+function updateTextUi(index) {
+	const activeDiv = colorDivs[index];
+	const color = chroma(activeDiv.style.backgroundColor);
+	const textHex = activeDiv.querySelector("h2");
+	const icons = activeDiv.querySelectorAll(".controls button");
+	textHex.innerText = color.hex();
+	//Check Contrast
+	checkTextContrast(color, textHex);
+	{
+		for (icon of icons) {
+			checkTextContrast(color, icon);
+		}
+	}
 }
 randomColors();
