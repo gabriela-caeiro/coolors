90ed8bce5eecb784670f4ec5b1e0676a7829c6e4
Gabriela Caeiro <gabriela.caeiro.topa@gmail.com>
2023-05-20 17:25:19 +0200

    Pushing to local storage

--- app.js
+++ app.js
@@ -233,23 +233,49 @@ const closeSave = document.querySelector(".close-save");
 const saveContainer = document.querySelector(".save-container");
 const saveInput = document.querySelector(".save-container input");
 
-
 //Event Listeners
 
 saveBtn.addEventListener("click", openPalette);
 closeSave.addEventListener("click", closePalette);
+submitSave.addEventListener("click", savePalette);
 
 function openPalette(e) {
 	const popup = saveContainer.children[0];
 	saveContainer.classList.add("active");
 	popup.classList.add("active");
-}	
+}
 
 function closePalette(e) {
 	const popup = saveContainer.children[0];
 	saveContainer.classList.remove("active");
 	popup.classList.remove("active");
-}	
+}
 
+function savePalette(e) {
+	saveContainer.classList.remove("active");
+	popup.classList.remove("active");
+	const name = saveInput.value;
+	const colors = [];
+	currentHexes.forEach((hex) => {
+		colors.push(hex.innerText);
+	});
+	//Generate obect
+	let paletteNr = savedPalettes.length;
+	const paletteObj = { name, colors, nr: paletteNr };
+	savedPalettes.push(paletteObj);
+	//Save to local storage
+	savetoLocal(paletteObj);
+	saveInput.value = "";
+}
+function savetoLocal(paletteObj) {
+	let localPalettes;
+	if (localStorage.getItem("palettes") === null) {
+		localPalettes = [];
+	} else {
+		localPalettes = JSON.parse(localStorage.getItem("palettes"));
+	}
+	localPalettes.push(paletteObj);
+	localStorage.setItem("palettes", JSON.stringify(localPalettes));
+}
 
 randomColors();
